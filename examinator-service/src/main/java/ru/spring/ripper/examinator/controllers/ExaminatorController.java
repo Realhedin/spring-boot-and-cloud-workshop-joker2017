611fb75b2a068e53a41554d53a7a293e9ae451c9
dkorolev <korwork87@yahoo.com>
2018-09-07 12:37:17 +0300

    added additional methods

--- examinator-service/src/main/java/ru/spring/ripper/examinator/controllers/ExaminatorController.java
+++ examinator-service/src/main/java/ru/spring/ripper/examinator/controllers/ExaminatorController.java
@@ -1,8 +1,7 @@
 package ru.spring.ripper.examinator.controllers;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import ru.spring.ripper.model.CheckedExam;
 import ru.spring.ripper.model.SolvedExam;
 
@@ -12,11 +11,37 @@ import ru.spring.ripper.model.SolvedExam;
 @RestController
 public class ExaminatorController {
 
-    @RequestMapping("/examine")
+    //curl -X POST http://localhost:8080/examine -d '{"title" : "exam"}' -H "Content-Type: application/json"
+    @RequestMapping(path = "/examine", method = RequestMethod.POST)
     public CheckedExam examine(@RequestBody SolvedExam solvedExam) {
         return CheckedExam.builder()
                 .mark(5)
                 .title(solvedExam.getTitle())
+                .build();
+    }
+
+    //curl -X GET http://localhost:8080/examine -H "Content-Type: application/json"
+    @RequestMapping(path = "/examine", method = RequestMethod.GET)
+    public CheckedExam examine2() {
+        return CheckedExam.builder()
+                .mark(2)
+                .build();
+    }
+
+    //curl -X GET http://localhost:8080/examine2 -H "Content-Type: application/json"
+    @GetMapping(path = "/examine2", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CheckedExam examine3() {
+        return CheckedExam.builder()
+                .mark(2)
+                .build();
+    }
+
+    // curl -X GET http://localhost:8080/examnin3/curl?debug=true -H "Content-Type: application/json"
+    @GetMapping(path = "/examine3/{title}")
+    public CheckedExam examine2(@PathVariable String title, @RequestParam Boolean debug) {
+        return CheckedExam.builder()
+                .mark(3)
+                .title(title)
                 .build();
     }
 }
