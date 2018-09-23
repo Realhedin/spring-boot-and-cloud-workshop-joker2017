aeb8d3c448a4e74db2744cd1ba5accde5e5617ac
dkorolev <korwork87@yahoo.com>
2018-09-23 14:36:40 +0300

    added @RequireArgsConstructor to inject correctly. added default value.

--- theology-service/src/main/java/ru/spring/ripper/theology/controller/RandomExerciesController.java
+++ theology-service/src/main/java/ru/spring/ripper/theology/controller/RandomExerciesController.java
@@ -1,6 +1,6 @@
 package ru.spring.ripper.theology.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +12,19 @@ import java.util.List;
 import java.util.Random;
 
 /**
+ * Simple rest controller for hadling requests.
+ *
  * Created by dkorolev on 9/17/2018.
  */
 @RestController
+@RequiredArgsConstructor //allows no to set autowire on field, because it creates Constructor with setting repository.
+                         //It's better practice than inject fields (no curcuit dependency). Lombok annotation.
 public class RandomExerciesController {
 
-    @Autowired
     private ExerciseRepository repository;
 
     @GetMapping("/exercise/random")
-    public List<Exercise> randomExam(@RequestParam int count) {
+    public List<Exercise> randomExam(@RequestParam(defaultValue = "5") int count) {
         List<Exercise> all = repository.findAll();
         List<Exercise> exercises = new ArrayList<>(3);
         Random random = new Random();
