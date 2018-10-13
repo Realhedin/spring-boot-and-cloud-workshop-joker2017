cf398ab105a2bee721012e31efeab68df86bf286
dkorolev <korwork87@yahoo.com>
2018-10-13 20:24:07 +0300

    added sql files (not inserted in table) make field final to remove Autowired above it

--- theology-service/src/main/resources/data.sql
+++ theology-service/src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO EXERCISE (ID,QUESTION, ANSWER) VALUES (null,'question1','answer1');
-INSERT INTO EXERCISE (`id`,`question`, `answer`) VALUES (null,'question2', 'answer2');
+INSERT INTO EXERCISE (ID,QUESTION, ANSWER) VALUES (null,'question2', 'answer2');
