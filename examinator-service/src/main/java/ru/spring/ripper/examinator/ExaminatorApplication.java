dad4fd0bbbf303eb87d9f3bce025e5594bf41a24
dkorolev <korwork87@yahoo.com>
2018-11-25 15:24:15 +0300

    remove ExternalServiceProperties as not needed after enabling Eureka

--- examinator-service/src/main/java/ru/spring/ripper/examinator/ExaminatorApplication.java
+++ examinator-service/src/main/java/ru/spring/ripper/examinator/ExaminatorApplication.java
@@ -2,7 +2,6 @@ package ru.spring.ripper.examinator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -11,7 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableConfigurationProperties(ExternalServiceProperties.class)
 public class ExaminatorApplication {
   public static void main(String[] args) {
     SpringApplication.run(ExaminatorApplication.class, args);
